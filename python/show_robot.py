fa68a8f91062b84c4156963b3c236f9b8f9ee7ca
German Espinosa <germanespinosa@gmail.com>
2021-12-09 10:18:40 -0600

    new version

--- python/show_robot.py
+++ python/show_robot.py
@@ -1,11 +1,6 @@
-import time
-
-import matplotlib.pyplot as plt
-
 from cellworld_py import *
 from agent_tracking_py import Agent_tracking, Filter
 import subprocess
-import matplotlib as mpl
 
 tracker = Agent_tracking()
 tracker.filter = Filter(.8, 10, 5)
@@ -28,13 +23,6 @@ display = Display(world, animated=True)
 
 robot = tracker.current_states["predator"].copy()
 
-# tx = []
-# ty = []
-
-# tx.append(robot.location.x)
-# ty.append(robot.location.y)
-# trajectory, = display.ax.plot(tx, ty, 'r-')# Returns a tuple of line objects, thus the comma
-
 def onclick(event):
     location = Space.transform_to(Location(event.xdata, event.ydata), src_space, dst_space)
     cell_id = mice_world.cells.find(location)
@@ -51,11 +39,7 @@ cid = display.fig.canvas.mpl_connect('button_press_event', onclick)
 while t:
     robot = tracker.current_states["predator"].copy()
     display.agent(step=tracker.current_states["predator"], color="dimgray", show_trajectory=True)
-    # tx.append(robot.location.x)
-    # ty.append(robot.location.y)
     rotation = robot.rotation
-    # trajectory.set_xdata(tx)
-    # trajectory.set_ydata(ty)
     display.update()
 
 tracker.unregister_consumer()
