96535f1e7ff1e8892cbda31e5943eee27ced7754
gabwink <gabriellewink2025@u.northwestern.edu>
2022-03-30 07:45:05 -0500

    test buttons gamepad

--- src/tool/hybrid_gamepad.cpp
+++ src/tool/hybrid_gamepad.cpp
@@ -16,16 +16,16 @@ using namespace gamepad;
 
 int main(){
     cout << "hi" << endl;
-    string robot_ip = "192.168.137.155";
+//    string robot_ip = "192.168.137.155";
     string device = "/dev/input/js0";
 
-    // connect to robot
-    Robot robot;
-    if (!robot.connect(robot_ip))
-    {
-        cout << "Unable to connect to robot." <<  endl;
-        exit(0);
-    }
+//    // connect to robot
+//    Robot robot;
+//    if (!robot.connect(robot_ip))
+//    {
+//        cout << "Unable to connect to robot." <<  endl;
+//        exit(0);
+//    }
 
     Gamepad *j;
     j = new Gamepad_wrapper(device);
@@ -37,35 +37,35 @@ int main(){
     bool update;
 
     while (h){
-        update = false;
+//        update = false;
+//
+//        float left = -j->axes[1];
+//        float right = -j->axes[4];
+//
+//
+//        // modify speeds to robot range
+//        if (left > 0){
+//            left = abs((float)(left/JOYSTICK_MAX)) * (MAX - MIN) + MIN;;
+//        } else if (left < 0){
+//            left = -(abs((float)(left/JOYSTICK_MAX)) * (MAX - MIN) + MIN);
+//        }
+//        if (right > 0){
+//            right = abs((float)(right/JOYSTICK_MAX)) * (MAX - MIN) + MIN;
+//        } else if (right < 0){
+//            right = -(abs((float)(right/JOYSTICK_MAX)) * (MAX - MIN) + MIN);
+//        }
 
-        float left = -j->axes[1];
-        float right = -j->axes[4];
 
-
-        // modify speeds to robot range
-        if (left > 0){
-            left = abs((float)(left/JOYSTICK_MAX)) * (MAX - MIN) + MIN;;
-        } else if (left < 0){
-            left = -(abs((float)(left/JOYSTICK_MAX)) * (MAX - MIN) + MIN);
-        }
-        if (right > 0){
-            right = abs((float)(right/JOYSTICK_MAX)) * (MAX - MIN) + MIN;
-        } else if (right < 0){
-            right = -(abs((float)(right/JOYSTICK_MAX)) * (MAX - MIN) + MIN);
-        }
-
-
-        if (pleft != left || pright != right) {
-            robot.set_left(left);
-            robot.set_right(right);
-            update = true;
-            //cout << "left: "<< -j->axes[1] << " right: "<<  -j->axes[4]<< endl;
-            cout << "left: " << left << " right: " << right << endl;
-        }
-
-        pleft = left;
-        pright = right;
+//        if (pleft != left || pright != right) {
+//            robot.set_left(left);
+//            robot.set_right(right);
+//            update = true;
+//            //cout << "left: "<< -j->axes[1] << " right: "<<  -j->axes[4]<< endl;
+//            cout << "left: " << left << " right: " << right << endl;
+//        }
+//
+//        pleft = left;
+//        pright = right;
 
 
 //        if ( j->buttons[5].state == 1 ){
@@ -74,22 +74,22 @@ int main(){
 //        }
 
 
-
-
-        if (update) {
-            robot.update();
-        }
+//
+//
+//        if (update) {
+//            robot.update();
+//        }
 
         // test all joystick inputs
 //        for (int i = 0;i<j->axes.size();i++) {
 //            if (j->axes[i])
 //                cout << "axis " << i << ": " << j->axes[i] << endl;
 //        }
-//         for (int i = 0;i<j->buttons.size();i++) {
-//             if (j->buttons[i].state == 1)
-//                 cout << "button " << i << ": " << j->buttons[i].state << "\t";
-//        }
-
+         for (int i = 0;i<j->buttons.size();i++) {
+             if (j->buttons[i].state == 1)
+                 cout << "button " << i << ": " << j->buttons[i].state << endl;
+        }
+        cout << "button 7: " << j -> buttons[7].state << endl;
         usleep(30000);
         if (j->buttons[8].state == 1) h = false; // exit loop
     }
