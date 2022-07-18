f6aecbff1bc0779f49e4e733743510ea9375f36f
germanespinosa <germanespinosa@gmail.com>
2022-07-18 11:39:42 -0500

    replaced dice_double with dice_float

--- src/tracking_simulator.cpp
+++ src/tracking_simulator.cpp
@@ -11,11 +11,11 @@ namespace robot {
         if (Chance::coin_toss(frame_drop)) return true; //send 95% of the updates (simulates missing frames)
         auto transformed_step = step;
         //simulates noise
-        transformed_step.location.x += Chance::dice_double(-noise, noise);
-        transformed_step.location.y -= Chance::dice_double(-noise, noise);
+        transformed_step.location.x += Chance::dice_float(-noise, noise);
+        transformed_step.location.y -= Chance::dice_float(-noise, noise);
         // simulates bad readings
         if (Chance::coin_toss(bad_reads)) { //1% of the updates are garbage
-            transformed_step.location = Location(Chance::dice_double(0, 1),Chance::dice_double(0, 1));
+            transformed_step.location = Location(Chance::dice_float(0, 1),Chance::dice_float(0, 1));
         }
         send_step(transformed_step);
         return true;
