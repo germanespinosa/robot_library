93060275983dd0428ff1c27bb64929bd16986f89
gabwink <gabriellewink2025@u.northwestern.edu>
2022-08-03 08:15:00 -0500

    move3 move trainer added

--- python/move_optimzer.py
+++ python/move_optimzer.py
@@ -1,8 +1,33 @@
+'''
+TO DO:
+1. mode 1 initialize robot
+    a. PD to desired angle  to get to destination
+    b. PD to move fwd
+    c. or combine like old controller
+2. mode 2 tune moves
+    a. from center of cell to start pos
+    b. 6 moves
+3. controller
+
+
+- if tick num increase then decrease oscillation in modified value --> decrease step
+- save new ticks to file
+- tune all  moves in sequence
+- refine tuning to precise, optimal tick
+'''
+
+
 import math
 from time import sleep
-from cellworld import World, Display, Location, Agent_markers, Capture, Capture_parameters, Step, Timer, Cell_group_builder, to_radians, to_degrees, Location_list, Cell_group, Cell_map
+from cellworld import World, Display, Location, Agent_markers, Step, Timer, Cell_map, Coordinates
 from cellworld_tracking import TrackingClient
 from tcp_messages import MessageClient, Message
+import matplotlib.pyplot as plt
+
+# GLOBALS
+DIRECTION_X = 1
+MOVE_TUNED = False
+PREVIOUS_LOCATION = None
 
 
 class AgentData:
@@ -13,6 +38,8 @@ class AgentData:
         self.is_valid = None
         self.step = Step()                      # step object - step is a class in experiment.py
         self.step.agent_name = agent_name
+        self.move_state = None
+        self.move_done = None
 
 
 def on_step(step):
@@ -25,6 +52,16 @@ def on_step(step):
 
 
 class Robot_client(MessageClient):
+
+    def __init__(self):
+        MessageClient.__init__(self)
+        self.move_done = None
+        self.router.add_route("move_done", self.__move_done__, int)
+
+    def __move_done__(self, move_number):
+        if self.move_done:
+            self.move_done(move_number)
+
     def set_left(self, v: int) -> bool:
         return self.send_request(Message("set_left", v)).get_body(bool)
 
@@ -46,8 +83,8 @@ def initialize():
 
 
 def robot_tick_update(left_tick, right_tick):
-    robot_client.set_left(300)
-    robot_client.set_right(300)
+    robot_client.set_left(left_tick)
+    robot_client.set_right(right_tick)
     robot_client.set_speed(robot_speed)
     robot_client.update()
 
@@ -73,11 +110,13 @@ def normalize_radians(angle):
         angle += 2 * math.pi
     while angle > 2*math.pi:
         angle -= 2 * math.pi
+    return angle
 
 def angle_difference(a1, a2):
     """
     Return smallest angle difference
     """
+
     a1 = normalize_radians(a1)
     a2 = normalize_radians(a2)
     if a1 > a2:
@@ -85,7 +124,7 @@ def angle_difference(a1, a2):
         if dth < math.pi:
             return dth
         else:
-            return a2 + math.pi *2 - a1
+            return a2 + math.pi * 2 - a1
     else:
         dth = a2 - a1
         if dth < math.pi:
@@ -126,15 +165,113 @@ def robot_mode_1(position, destination):
     else:
         return False
 
-# Globals
+
+def turn_robot():
+    global DIRECTION_X
+    if DIRECTION_X > 0 and predator.step.location.x > 0.9:
+        DIRECTION_X = -1
+        predator.move_done = False
+        robot_tick_update(tick_guess_dict["m6"]['L'], tick_guess_dict["m6"]['R'])
+    elif DIRECTION_X < 0 and predator.step.location.x < -0.9:
+        DIRECTION_X = 1
+        predator.move_done = False
+        robot_tick_update(tick_guess_dict["m6"]['L'], tick_guess_dict["m6"]['R'])
+
+def move_done(move_number):
+    # print("move ", move_number, "done")
+    # display.circle(predator.step.location)
+    # display.update()
+
+    predator.move_state = move_number
+    # when this function is called move state changes
+    predator.move_done = True
+
+def get_location(x,y):
+    return world.cells[map[Coordinates(x,y)]].location
+
+def tune_move6():
+    pass
+
+def tune_move3():
+    global MOVE_TUNED
+    global PREVIOUS_LOCATION
+
+    display.circle(predator.step.location, 0.01, "red")
+    predator.move_done = False
+    # take measurement
+    current_location = predator.step.location
+
+    # find error
+    distance = current_location.dist(PREVIOUS_LOCATION)
+    error = STRAIGHT - distance         # desired - actual
+
+    # tick modification logic
+    if abs(error) <= 0.001:
+        print(f"Distance: {distance}, Ticks: {tick_guess_dict['m3']}")
+        print("DONE")
+        MOVE_TUNED = True
+    else:
+        tick_guess_dict[move]['L'] += P * error
+        tick_guess_dict[move]['R'] += P * error
+
+        robot_tick_update(tick_guess_dict[move]['L'], tick_guess_dict[move]['R'])
+        print(f"Desired: {STRAIGHT}, Distance: {distance}, TICKS: {tick_guess_dict[move]['L']}")
+
+    # record previous location
+    PREVIOUS_LOCATION = current_location
 
 
-# Variables
-time_out = 1.0
+
+
+# CONSTANTS
+R1 = 0.0635/2
+R2 = R1 + 0.11/2.54
+TH1 = 120
+TH2 = 60
+TH3 = 180
+STRAIGHT = 0.11
+time_out = 2.0
 robot_speed = 100
 
+
+# GLOBALS
+# the desired change in x,y,th for each move
+# delta_dict = { "m1" :  np.array([,y1, 120]),
+#                "m2" :  np.array([x2, y2, 60]),
+#                "m3" :  np.array([23, 23, 0]),
+#                "m4" :  np.array([x2, -y2, -60]),
+#                "m5" :  np.array([x1, -y1, -120]),
+#                "m6" :  np.array([0, 0, 180])}
+# initial tick guess
+tick_guess_dict =  {"m1": {'L': 33, 'R': 323},
+                    "m2": {'L': 128, 'R': 306},
+                    "m3": {'L': 216, 'R': 216},
+                    "m4": {'L': 306, 'R': 128},
+                    "m5": {'L': 323, 'R':-33},
+                    "m6": {'L': 840, 'R': -840},
+                    "m7": {'L': 420, 'R': -420}}
+# move characteristic dict
+move_constants_dict= {"m1": {'r': R1,       'th': TH1},
+                      "m2": {'r': R2,       'th': TH2},
+                      "m3": {'r': STRAIGHT, 'th': 0},
+                      "m4": {'r': R2,       'th': -TH2},
+                      "m5": {'r': R1,       'th': -TH1},
+                      "m6": {'r': 0,        'th': 180},
+                      "m7": {'r': 0,        'th': 90}}
+# function association dict
+function_dict =  {  "m1": 0,
+                    "m2": 0,
+                    "m3": tune_move3,
+                    "m4": 0,
+                    "m5": 0,
+                    "m6": 0,
+                    "m7": 0 }
+moves = ["m1", "m2", "m3", "m4", "m5", "m6", "m7"]
+move = moves[2]
+
+
 # Setup
-occlusions = "20_05"
+occlusions = "00_00"
 world = World.get_from_parameters_names("hexagonal", "canonical", occlusions)
 display = Display(world, fig_size=(9.0*.75, 8.0*.75), animated=True)
 map = Cell_map(world.configuration.cell_coordinates)
@@ -159,6 +296,9 @@ if robot_client.connect("127.0.0.1", 6300):
 else:
     print("failed to connect to robot! bummer")
     exit(1)
+# robot client broadcasts when the move is done
+robot_client.move_done = move_done
+robot_client.subscribe()
 
 # Tasks
 # 1. initialize robot
@@ -168,34 +308,40 @@ prev_error = 0
 P = 1
 D = 1
 mode1 = False
+
+
+# MOVE TUNER
+previous_theta = 0
+direction = 'cw'
+count = 0           # tuning is adequate if meet count requirement
+step0_done = False
+# current_theta = 0
+TH = 90 # angle to tune
+theta_accuracy = 0
+PREVIOUS_LOCATION = get_location(0,0)
+print(PREVIOUS_LOCATION)
+STRAIGHT = (0.11/2)/2.34
+distance_accuracy = 0
+
+# Try move
+robot_tick_update(tick_guess_dict[move]['L'], tick_guess_dict[move]['R'])
+
+
+P = 100  #500 #1000
+loop_count = 0
+dist_list = []
+count_list = []
+
 while True:
-    # get robot to start position
-    while False:
+    # turn predator around to keep it in bounds
+    if predator.move_done:
+        turn_robot()
 
-        destination = get_center_location(predator.step.location)
-        dist = predator.step.location.dist(destination)
-        destination_theta = predator.step.location.atan(destination)# atan
-        theta = to_radians(predator.step.rotation)
-        error = angle_difference(theta, destination_theta) * turn_direction(theta, destination_theta)
-        normal_error = normalize_error(error)
-        error_derivative = (error - prev_error)
-        prev_error = error
-        adjustment = error * P + D * error_derivative
-        #left_tick = normal_error *
-        robot_tick_update(left_tick, right_tick)
-        robot_mode_1(predator.step.location, destination)
+    # send new ticks if previous move is done
+    if predator.move_done and not MOVE_TUNED:
+        function_dict[move]()
 
-    # move robot to init location
 
-    destination = get_center_location(predator.step.location)
-    dist = predator.step.location.dist(destination)
-    destination_theta = predator.step.location.atan(destination)# atan
-    theta = to_radians(predator.step.rotation)
-    error = angle_difference(theta, destination_theta) * turn_direction(theta, destination_theta)
-    normal_error = normalize_error(error)
-    error_derivative = (error - prev_error)
-    prev_error = error
-    adjustment = error * P + D * error_derivative
 
 
 
@@ -206,10 +352,8 @@ while True:
     else:
         display.agent(step=predator.step, color="grey", size= 15)
 
-    display.circle(start_destination, 0.01, 'red')
-
     display.update()
-    sleep(0.1)
+    sleep(0.5)
 
 tracker.unsubscribe()
 
